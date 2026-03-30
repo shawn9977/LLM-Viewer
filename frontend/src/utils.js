852344a88a1e7c084e243a36ba182c67e3766b21
shawn9977 <shawn.zhao@intel.com>
2026-03-30 17:08:06 +0800

    front page Format to two decimal places

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,21 +2,21 @@ export function strNumber(num, unit='') {
   if (num > 1e14) {
     return `${(num / 1e12).toFixed(0)}T${unit}`;
   } else if (num > 1e12) {
-    return `${(num / 1e12).toFixed(1)}T${unit}`;
+    return `${(num / 1e12).toFixed(2)}T${unit}`;
   } else if (num > 1e11) {
     return `${(num / 1e9).toFixed(0)}G${unit}`;
   } else if (num > 1e9) {
-    return `${(num / 1e9).toFixed(1)}G${unit}`;
+    return `${(num / 1e9).toFixed(2)}G${unit}`;
   } else if (num > 1e8) {
     return `${(num / 1e6).toFixed(0)}M${unit}`;
   } else if (num > 1e6) {
-    return `${(num / 1e6).toFixed(1)}M${unit}`;
+    return `${(num / 1e6).toFixed(2)}M${unit}`;
   } else if (num > 1e5) {
     return `${(num / 1e3).toFixed(0)}K${unit}`;
   } else if (num > 1e3) {
-    return `${(num / 1e3).toFixed(1)}K${unit}`;
+    return `${(num / 1e3).toFixed(2)}K${unit}`;
   } else if (num >= 1) {
-    return `${num.toFixed(1)}${unit}`;
+    return `${num.toFixed(2)}${unit}`;
   } else {
     return `${num.toFixed(2)}${unit}`;
   }
@@ -24,13 +24,13 @@ export function strNumber(num, unit='') {
 
 export function strNumberTime(num) {
   if (num >= 1) {
-    return `${num.toFixed(1)}s`;
+    return `${num.toFixed(2)}s`;
   } else if (num > 1e-3) {
-    return `${(num * 1e3).toFixed(1)}ms`;
+    return `${(num * 1e3).toFixed(2)}ms`;
   } else if (num > 1e-6) {
-    return `${(num * 1e6).toFixed(1)}us`;
+    return `${(num * 1e6).toFixed(2)}us`;
   } else if (num > 1e-9) {
-    return `${(num * 1e9).toFixed(1)}ns`;
+    return `${(num * 1e9).toFixed(2)}ns`;
   } else {
     return `${num.toFixed(0)}s`;
   }
